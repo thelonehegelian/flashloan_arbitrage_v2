1cdde9bc1451c1259ee786914e89c4f0097373b1
thelonehegelian <thelonehegelian@gmail.com>
2023-02-21 11:21:52 +0000

    Update interfaces

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,33 @@
-import "@nomiclabs/hardhat-waffle"
-import '@nomiclabs/hardhat-ethers'
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-nodemon";
 
-
-require('dotenv').config()
+require("dotenv").config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
- const environment = {
-
+const environment = {
   chainId: 31337, // chain ID for hardhat testing
   NODE_ENV: process.env.ARCHIVE_NODE,
-  blockNumber: 14390000
+  blockNumber: 14390000,
 };
 
-
-
 module.exports = {
-
   solidity: {
     compilers: [
-      {version: "0.6.12"},
-      {version: "0.7.6"},
-      {version: "0.5.16"}
-    ]
+      { version: "0.6.12" },
+      { version: "0.7.6" },
+      { version: "0.5.16" },
+    ],
   },
-    networks: {
-      hardhat: {
-        chainId: environment.chainId,
-        forking: {
-          
-            url: environment.NODE_ENV,        
-            blockNumber: environment.blockNumber     
-        }
-      }
-  
-    }
+  networks: {
+    hardhat: {
+      chainId: environment.chainId,
+      forking: {
+        url: environment.NODE_ENV,
+        blockNumber: environment.blockNumber,
+      },
+    },
+  },
 };
